2d8211c1107f791075dbe5c8bc303b544ebc1dc7
oldking <mortimer2015@hotmail.com>
2018-09-15 03:57:31 +0800

    review garbage code

--- apps/spiders/ygdy8_spider.py
+++ apps/spiders/ygdy8_spider.py
@@ -7,15 +7,16 @@ from scrapy.spiders import CrawlSpider, Rule
 
 from apps import settings
 from helpers.dingtalk import send_text
+from helpers.tools import spider_get_data_path
 
-name_compile = compile(r'\[[0-9]{2}\.[0-9]{2}\].*')
-name1_compile = compile(r'^\s*\[.*\]\s*$')
+name_start_time_compile = compile(r'\[[0-9]{2}\.[0-9]{2}\].*')
+name_compile = compile(r'^\s*\[.*\]\s*$')
 url_xpath = '//tbody/tr/td/a/@href'
 movie_detail_view_xpath = '//td/b/a'
 site_domains = 'www.ygdy8.com'
 
 
-class EasySpider(CrawlSpider):
+class Ygdy8_Spider(CrawlSpider):
     name = 'ygdy8_spider'
     allowed_domains = [site_domains]
     start_urls = [
@@ -30,10 +31,10 @@ class EasySpider(CrawlSpider):
         # hardcode
         movie_info = split('(<br>|<p>)', response.xpath('//p').extract()[4])
         info = ''
-        with open(site_domains, 'r+') as f:
+        with open(spider_get_data_path(site_domains), 'r+') as f:
             already_download_movie_name = set(f.readlines())
             for line in movie_info:
-                if name_compile.search(line) or name1_compile.search(line):
+                if name_start_time_compile.search(line) or name_compile.search(line):
                     if '{}\n'.format(line) in already_download_movie_name:
                         break
                     else:
@@ -43,10 +44,7 @@ class EasySpider(CrawlSpider):
             else:
                 url = response.xpath(url_xpath).extract()[0]
                 info = '{}\n\n下载链接：{}\n\n详细信息页：{}'.format(
-                    info,
-                    url,
-                    response.url
-                )
+                    info, url, response.url)
                 # print info
                 print(info)
                 send_text(info, to=settings.DINGTALK_TOKEN)
