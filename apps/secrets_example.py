aefd1861aad2f7d8bf39a4996a3ab9931de0b108
oldking <mortimer2015@hotmail.com>
2018-09-15 12:15:31 +0800

    bug fix

--- apps/secrets_example.py
+++ apps/secrets_example.py
@@ -4,5 +4,5 @@ import os
 # set token of dingtalk robot send movie info
 DINGTALK_TOKEN = 'YOUR DINGTALK TOKEN'
 
-# set file of already spider link
+# set file of already spider link, if None data will write to 'pwd'
 DATA_DIR = None
